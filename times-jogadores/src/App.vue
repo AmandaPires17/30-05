443e5a95329f788113b69eba71c62a24c4437e3f
AmandaPires <102668847+AmandaPires17@users.noreply.github.com>
2022-05-30 15:12:41 -0300

    Add files via upload

---
+++ times-jogadores/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>Home | Jogadore | Times</header>
+  <main>Main</main>
+  <footer>Rodapé</footer>
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  /*padding: 2rem; */
+  height: 100vh;
+  font-weight: normal;
+  background-color: pink;
+}
+
+header,
+footer {
+  background-color: purple;
+  color: white;
+  height: 15%;
+  font-size: 1.3rem;
+  display: flex;
+  align-items: center;
+  padding-left: 2rem;
+}
+
+main {
+  height: 70%;
+}
+</style>
